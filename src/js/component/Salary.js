fea0c56a15cb72acbd833194a3947d060b398fb4
Mauro2028 <mauriciolizcano25531@gmail.com>
2021-03-12 14:14:42 +0000

    hecho el filtro

--- src/js/component/Salary.js
+++ src/js/component/Salary.js
@@ -13,9 +13,11 @@ const Workers = () => {
 	const [filter, setFilter] = useState("");
 
 	const handleChange = e => {
-		setFilter(e.filter.name);
+		setFilter({
+			...filter,
+			[e.target.name]: e.target.value
+		});
 		console.log(filter);
-		console.log("-->", filter);
 	};
 
 	const { store, actions } = useContext(Context);
@@ -25,17 +27,45 @@ const Workers = () => {
 		<div>
 			<div className="container">
 				<div>
-					<input type="text" onChange={e => setFilter(handleChange({ filter }))} />
+					{/* <input type="text" onChange={e => setFilter(handleChange({ filter }))} /> */}
+					<select>
+						<option
+							className={filter.name == "Tecnologico"}
+							name="Tecnologico"
+							onClick={e => setFilter({ name: e.target.name })}>
+							Sector Tecnologico
+						</option>
+						<option
+							className={filter.name == "Financiero"}
+							name="Financiero"
+							onClick={e => setFilter({ name: e.target.name })}>
+							Sector Financiero
+						</option>
+					</select>
+
+					{/* <button
+						className={filter.name === "Tecnologico"}
+						name="Tecnologico"
+						onClick={e => setFilter({ name: e.target.name })}>
+						Sector Tecnologico
+					</button>
+					<button
+						className={filter.name === "Financiero"}
+						name="Financiero"
+						onClick={e => setFilter({ name: e.target.name })}>
+						Sector Financiero
+					</button> */}
 					<div id="workers" className="panel-collapse collapse show" aria-expanded="true">
 						<ul className="list-group pull-down" id="contact-list">
-							{store.workers.filter(worker => worker.candidate.includes(filter)).map(worker => (
-								<SalaryCard
-									key={worker.id}
-									name={worker.candidate}
-									value={worker.sector}
-									worker={worker}
-									onDelete={() => setState({ showModal: true, idToDelete: contact.id })}
-								/>
+							{store.workers.filter(worker => worker.sector == filter.name).map(worker => (
+								<li id="worker" key={worker.id}>
+									<SalaryCard
+										name={worker.candidate}
+										value={worker.sector}
+										worker={worker}
+										onDelete={() => setState({ showModal: true, idToDelete: contact.id })}
+									/>
+								</li>
 							))}
 						</ul>
 
--- src/js/component/Worker-list.js
+++ src/js/component/Worker-list.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { SalaryCard } from "./SalaryCard";
 import { Modal } from "../component/Modal";
 import { Context } from "../store/appContext.js";
 
@@ -9,9 +9,6 @@ const WorkersList = () => {
 		showModal: false,
 		idToDelete: null
 	});
-	// const vsa = store.workers.variable_salary * 12;
-	// const sbf = workers.basic_salary * worker.Factor;
-	// const cta = workers.cesta_ticket * 12;
 
 	const { store, actions } = useContext(Context);
 	// debugger;
@@ -31,115 +28,11 @@ const WorkersList = () => {
 									worker => (
 										console.log(worker.candidate),
 										(
-											<li>
-												<div className="list-group-item">
-													<div className="row w-100">
-														<div className="col-12 col-sm-6 col-md-3 px-0" />
-														<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
-															<label className="text small text-truncate">
-																Numero: {worker.id}
-															</label>
-
-															<br />
-															<label className="text small text-truncate">
-																fecha de inicio: {worker.init_date}
-															</label>
-															<br />
-
-															<label className="text small text-truncate">
-																Consultor: {worker.Consultor}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Nombre del candidato: {worker.candidate}
-															</label>
-
-															<br />
-															<label className="text small text-truncate">
-																Numero de cedula: {worker.cedula}
-															</label>
-															<br />
-
-															<label className="text small text-truncate">
-																Status: {worker.status}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Numero de telefono: {worker.phone_number}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Email: {worker.email}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Fuente de captacion: {worker.catchment_source}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Gerencia: {worker.managment}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Vacante: {worker.vacant}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Fecha de cita a entrevista: {worker.interview_date}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Cargo actual: {worker.actual_charge}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Empresa: {worker.company}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Sector: {worker.sector}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Moneda extranjera: {worker.coin}
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Sueldo basico: {worker.basic_salary} Bs
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Sueldo Variable : {worker.variable_salary} Bs
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Cesta ticket: {worker.cesta_ticket} Bs
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Dias de utilidades: {worker.Profit_Days} dias
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Disfrute de vacaciones: {worker.vacations} dias
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Bono de vacaciones: {worker.Vacation_Bonus} Bs
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Factor: {worker.Factor} Bs
-															</label>
-															<br />
-															<label className="text small text-truncate">
-																Paquete anual estimado:{" "}
-																{worker.Estimated_annual_package}$
-															</label>
-														</div>
-													</div>
-												</div>
-											</li>
+											<SalaryCard
+												key={worker.id}
+												worker={worker}
+												onDelete={() => setState({ showModal: true, idToDelete: contact.id })}
+											/>
 										)
 									)
 								)}
